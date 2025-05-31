43f40b963fea79d67f7307abf9f3760b3f4e86e2
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-06-01 00:19:06 +0800

    test : testing JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello!")
+function getComputerChoice() {
+    const options = [`rock`, `paper`, `scissors`];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+
+function playRound(playerChoice, computerChoice) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It`s a tie!"
+    }
